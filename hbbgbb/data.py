2c54caec3fde6e258ba8289d83d2307b0f5c8e78
Karol Krizka <kkrizka@gmail.com>
2021-12-26 20:59:25 -0800

    More improvements to model_graph.

--- hbbgbb/data.py
+++ hbbgbb/data.py
@@ -1,10 +1,22 @@
 import glob
 import settings
+import numpy as np
 import pandas as pd
+import h5py
+import itertools
+import tqdm
+import graph_nets as gn
 
-def load_data():
+def load_data(tag='r10201'):
+    """
+    Load fat jet data into a Dataframe. Basic pre-selection is applied.
+
+    Parameters
+    --
+        `tag`: str, tag of dataset to use
+    """
     # Load the data
-    path=glob.glob(f'{settings.datadir}/user.zhicaiz.309450.NNLOPS_nnlo_30_ggH125_bb_kt200.hbbTrain.e6281_s3126_r10201_p4258.2020_ftag5dev.v0_output.h5/*.output.h5')[0]
+    path=glob.glob(f'{settings.datadir}/user.zhicaiz.309450.NNLOPS_nnlo_30_ggH125_bb_kt200.hbbTrain.e6281_s3126_{tag}_p4258.2020_ftag5dev.v0_output.h5/*.output.h5')[0]
     df=pd.read_hdf(path,key='fat_jet')
 
     # Apply preselection
@@ -17,7 +29,11 @@ def load_data():
     return df
 
 def label(df):
-    """ Add `label` inplace column to dataframe `df`. """
+    """
+    Decorate a fat jet `df` DataFrame with labels.
+    - `label`: sparese label (0-2)
+    - `labelx`: one-hot label `x`
+    """
     df['label0']=(df.GhostHBosonsCount==1)
     df['label1']=(df.GhostHBosonsCount==0)&(df.GhostBHadronsFinalCount==2)
     df['label2']=(df.GhostHBosonsCount==0)&(df.GhostBHadronsFinalCount!=2)
@@ -26,3 +42,61 @@ def label(df):
     df.loc[df.label0,'label']=0
     df.loc[df.label1,'label']=1
     df.loc[df.label2,'label']=2
+
+def load_data_constit(tag='r10201'):
+    """
+    Load fat jet constituent data as a `h5py.Dataset`.
+
+    Parameters
+    --
+        `tag`: str, tag of dataset to use
+    """
+    path=glob.glob(f'{settings.datadir}/user.zhicaiz.309450.NNLOPS_nnlo_30_ggH125_bb_kt200.hbbTrain.e6281_s3126_{tag}_p4258.2020_ftag5dev.v0_output.h5/*.output.h5')[0]
+    f=h5py.File(path)
+    return f['fat_jet_constituents']
+
+def create_graph(fatjet,constit,feat=['pt','deta','dphi','trk_btagIp_d0','trk_btagIp_z0SinTheta','trk_btagIp_d0Uncertainty','trk_btagIp_z0SinThetaUncertainty']):
+    """
+    Create a dictionary graph for a large R jet. The graph is taken to be fully
+    connected. The node features are constituent properties listed in `feat`.
+
+    The `fatjet` is a `pd.Series` with information about the fat jet.
+
+    The `constit` is a structured array with information about the constituents.
+    """
+    # Global features are properties of the fat jet
+    globals=[]
+
+    # Nodes are individual tracks
+    nodes=np.array([constit[x] for x in feat]).T
+
+    # Fully connected graph, w/o loops
+    i=itertools.product(range(nodes.shape[0]),range(nodes.shape[0]))
+    senders=[]
+    receivers=[]
+    for s,r in i:
+        if s==r: continue
+        senders.append(s)
+        receivers.append(r)
+    edges=[[]]*len(senders)
+
+    return {'globals':globals, 'nodes':nodes, 'edges':edges, 'senders':senders, 'receivers':receivers}
+
+def create_graphs(fatjets, constits, feat):
+    """
+    Create fat jet graphs from a list of `fatjets` and their `constits`uents.
+    The `feat` is a list of constituent attributes to use as feature nodes.
+
+    The `fatjets` dataframe corresponds to fat jet properties. The index points
+    to the entry in `constits` corresponding to that jet.
+
+    The `constits` is a list of structured arrays for all fat jets. Each entry
+    contains the information of a constituent.
+    """
+    dgraphs=[]
+    constits=constits[fatjets.index.values]
+    for (i,fatjet),constit in tqdm.tqdm(zip(fatjets.iterrows(),constits),total=len(fatjets.index)):
+        constit=constit[~np.isnan(constit['pt'])]
+        dgraphs.append(create_graph(fatjet, constit, feat))
+
+    return gn.utils_tf.data_dicts_to_graphs_tuple(dgraphs)
--- model_graph.py
+++ model_graph.py
@@ -1,20 +1,14 @@
 # %%
-%load_ext autoreload
-%autoreload 2
+#%load_ext autoreload
+#%autoreload 2
 
 # %%
-import glob
-import time
-import itertools
 import tqdm
-
 import numpy as np
 import pandas as pd
-import networkx as nx
 import tensorflow as tf
 import graph_nets as gn
 import sonnet as snt
-import h5py
 import matplotlib.pyplot as plt
 
 import hbbgbb.plot as myplt
@@ -28,46 +22,24 @@ from hbbgbb import formatter
 fmt=formatter.Formatter('variables.yaml')
 
 # %% Load per jet information
-df=data.load_data()
-data.label(df)
+df_train=data.load_data()
+data.label(df_train)
+#df_train=df_train.loc[:1000].copy()
+
+df_test=data.load_data('r9364')
+data.label(df_test)
+#df_test=df_test.loc[:1000].copy()
 
 # %% Load jet constituent data
-path=glob.glob(f'{settings.datadir}/user.zhicaiz.309450.NNLOPS_nnlo_30_ggH125_bb_kt200.hbbTrain.e6281_s3126_r10201_p4258.2020_ftag5dev.v0_output.h5/*.output.h5')[0]
-f=h5py.File(path)
-fatjet_consts=f['fat_jet_constituents']
+fjc_train=data.load_data_constit()
+g_train=data.create_graphs(df_train, fjc_train)
 
-# %%
-def create_graph(fatjet,const):
-    # Global features are properties of the fat jet
-    globals=[]
+fjc_test=data.load_data_constit('r9364')
+g_test=data.create_graphs(df_test, fjc_test)
 
-    # Nodes are individual tracks
-    track_feat=['trk_btagIp_d0','trk_btagIp_z0SinTheta','trk_btagIp_d0Uncertainty','trk_btagIp_z0SinThetaUncertainty']
-    nodes=const[track_feat].to_numpy()
-
-    # Fully connected graph, w/o loops
-    i=itertools.product(range(nodes.shape[0]),range(nodes.shape[0]))
-    senders=[]
-    receivers=[]
-    for s,r in i:
-        if s==r: continue
-        senders.append(s)
-        receivers.append(r)
-    edges=[[]]*len(senders)
-
-    return {'globals':globals, 'nodes':nodes, 'edges':edges, 'senders':senders, 'receivers':receivers}
-
-mydf=df.copy()
-
-dgraphs=[]
-for i,fatjet in tqdm.tqdm(mydf.iterrows(),total=len(mydf.index)):
-    fatjet_const=pd.DataFrame(fatjet_consts[i])
-    fatjet_const=fatjet_const[~fatjet_const.pt.isna()]
-    dgraphs.append(create_graph(fatjet, fatjet_const))
-
-graphs=gn.utils_tf.data_dicts_to_graphs_tuple(dgraphs)
 #%%
-labels=tf.convert_to_tensor(mydf[['label0','label1','label2']])
+l_train=tf.convert_to_tensor(df_train[['label0','label1','label2']])
+l_test =tf.convert_to_tensor(df_test [['label0','label1','label2']])
 
 # %%
 class NormedMLP(snt.Module):
@@ -93,7 +65,7 @@ graph_network_2 = gn.modules.InteractionNetwork(
     node_model_fn=lambda: snt.nets.MLP([64]),
     edge_model_fn=lambda: snt.nets.MLP([64])
 )
-        
+
 graph_network_out = gn.modules.DeepSets(
     node_model_fn=lambda: snt.nets.MLP([64]),
     global_model_fn=lambda: snt.nets.MLP([64,2])
@@ -113,11 +85,21 @@ class Trainer:
         self.model= model
 
         # Training tools
-        self.stat = pd.DataFrame(columns=['loss'])
+        self.stat = pd.DataFrame(columns=['train_loss','test_loss'])
         self.opt  = snt.optimizers.Adam(learning_rate=0.1)
 
-    def step(self, graphs, labels):
+    def step(self, graphs, labels, g_test=None, l_test=None):
         """Performs one optimizer step on a single mini-batch."""
+        # Write test data
+        test_loss=0.
+        if g_test is not None:
+            pred = self.model(g_test)
+            logits=pred.globals
+            loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits,
+                                                            labels=l_test)
+            test_loss = tf.reduce_mean(loss)
+
+        # Training
         with tf.GradientTape() as tape:
             pred = self.model(graphs)
             logits=pred.globals
@@ -130,7 +112,7 @@ class Trainer:
         self.opt.apply(grads, params)
 
         # save training status
-        self.stat=self.stat.append({'loss':float(loss)}, ignore_index=True)
+        self.stat=self.stat.append({'train_loss':float(loss), 'test_loss':float(test_loss)}, ignore_index=True)
 
         return loss
 
@@ -140,68 +122,30 @@ t = Trainer(graph_network)
 fig_s,ax_s=plt.subplots(ncols=3,figsize=(24,8))
 fig_t,ax_t=plt.subplots(figsize=(8,8))
 
-epochs=20
-for epoch in range(epochs):
-    loss=float(t.step(graphs,labels))
+epochs=1000
+for epoch in tqdm.trange(epochs):
+    loss=float(t.step(g_train,l_train, g_test, l_test))
     print(epoch, loss)
 
-    pred=t.model(graphs)
-    mydf['pred']=tf.argmax(pred.globals, axis=1)
+    pred=t.model(g_test)
+    df_test['pred']=tf.argmax(pred.globals, axis=1)
     predsm=tf.nn.softmax(pred.globals)
-    mydf['score0']=predsm[:,0]
-    mydf['score1']=predsm[:,1]
-    mydf['score2']=predsm[:,2]
+    df_test['score0']=predsm[:,0]
+    df_test['score1']=predsm[:,1]
+    df_test['score2']=predsm[:,2]
 
     for i in range(3):
-        myplt.labels(mydf,f'score{i}','label',fmt=fmt, ax=ax_s[i])
-    fig_s.savefig(f'score_{epoch:08d}')
-    fig_s.clf()
+        ax_s[i].clear()
+        myplt.labels(df_test,f'score{i}','label',fmt=fmt, ax=ax_s[i])
+        ax_s[i].set_yscale('log')
+    fig_s.savefig('score')
 
-    ax_t.plot(t.stat.loss)
+    ax_t.clear()
+    ax_t.plot(t.stat.train_loss,label='Training')
+    ax_t.plot(t.stat.test_loss,label='Test')
     ax_t.set_yscale('log')
     ax_t.set_ylabel('loss')
-    #plt.ylim(1e-1, 1e2)
+    ax_t.ylim(1e-1, 1e3)
     ax_t.set_xlabel('epoch')
+    ax_t.legend()
     fig_t.savefig('training')
-    fig_t.clf()
-
-# %%
-plt.plot(t.stat.loss)
-plt.yscale('log')
-plt.ylabel('loss')
-#plt.ylim(1e-1, 1e2)
-plt.xlabel('epoch')
-plt.savefig('training.png')
-plt.show()
-plt.clf()
-
-# %%
-pred=graph_network(graphs)
-mydf['pred']=tf.argmax(pred.globals, axis=1)
-predsm=tf.nn.softmax(pred.globals)
-mydf['score0']=predsm[:,0]
-mydf['score1']=predsm[:,1]
-mydf['score2']=predsm[:,2]
-
-# %%
-myplt.labels(mydf,'score0','label',fmt=fmt)
-plt.savefig('score0')
-plt.show()
-plt.clf()
-# %%
-myplt.labels(mydf,'score1','label',fmt=fmt)
-plt.savefig('score1')
-plt.show()
-plt.clf()
-# %%
-myplt.labels(mydf,'score2','label',fmt=fmt)
-plt.savefig('score2')
-plt.show()
-plt.clf()
-
-# %%
-output='graph'
-analysis.roc(mydf, 'score0', f'roc_{output}')
-
-
-# %%
