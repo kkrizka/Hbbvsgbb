496362e33449f03870dccb56136757cb4684c95b
Karol Krizka <kkrizka@gmail.com>
2021-12-26 18:05:22 -0800

    Plot labels to predefined ax object.

--- hbbgbb/plot.py
+++ hbbgbb/plot.py
@@ -3,14 +3,17 @@ import matplotlib.pyplot as plt
 mylabels={0:'Higgs',1:'QCD (bb)', 2:'QCD (other)'}
 histcommon={'histtype':'step','density':True}
 
-def labels(df, varname, labelcol, predcol=None, fmt=None):
+def labels(df, varname, labelcol, predcol=None, fmt=None, ax=None):
+    if ax is None:
+        ax=plt.gca()
+
     histargs=fmt.hist(varname) if fmt is not None else {}
     histargs['density']=True
 
     for labelidx in sorted(mylabels.keys()):
         # Plot correctly labeled thing
         sdf=df[df[labelcol]==labelidx]
-        _,_,patch=plt.hist(sdf[varname],
+        _,_,patch=ax.hist(sdf[varname],
                     label=mylabels[labelidx] if predcol is None else None,
                     linestyle='--',
                     **histargs)
@@ -18,12 +21,12 @@ def labels(df, varname, labelcol, predcol=None, fmt=None):
         # Plot the predicted thing
         if predcol is not None:
             sdf=df[df[predcol]==labelidx]
-            plt.hist(sdf[varname],
+            ax.hist(sdf[varname],
                         label=mylabels[labelidx],
                         color=patch[0].get_edgecolor(),
                         **histargs)
 
-    plt.legend()
+    ax.legend()
 
-    fmt.subplot(varname, ax=plt.gca())
-    plt.ylabel('normalized')
+    fmt.subplot(varname, ax=ax)
+    ax.set_ylabel('normalized')
